1c942ddbf33e2b7b8a85c0f25340e8b19efdbfe4
Carolina Carranza <carolina@tallerest.com>
2024-04-25 22:00:40 -0600

    style: add TailwindCSS configurations and custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Inter'"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#23b3ab',
+          light: '#7cd4cc',
+          dark: '#04a49c',
+        },
+        secondary: {
+          DEFAULT: '#543c7c',
+          light: '#9c8cb4',
+          dark: '#3c2464',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
