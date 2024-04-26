b2875e5691ccf6745161339daa7e92814293ec36
Carolina Carranza <carolina@tallerest.com>
2024-04-26 12:13:04 -0600

    feat: add character details redirect functionality

--- src/components/cards/CharacterCard/CharacterCard.tsx
+++ src/components/cards/CharacterCard/CharacterCard.tsx
@@ -3,18 +3,25 @@ import { HeartIcon } from '@heroicons/react/24/outline';
 import toast from 'react-hot-toast';
 
 import { useFavoritesContext } from '../../../context/FavoritesContext/FavoritesContext';
-import { ICharacter } from '../../../interfaces/Character';
+import { ICharacter } from '../../../interfaces/api/Character';
 import womanImage from '../../../assets/woman.jpg';
 import manImage from '../../../assets/man.jpg';
 import { GENDERS } from '../../../enums/enums';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../../../router/routes';
+import { SyntheticEvent } from 'react';
 
 export const CharacterCard = ({ character }: PropTypes) => {
   const { favorites, addFavorite, removeFavorite } = useFavoritesContext();
+  const navigate = useNavigate();
 
   const silhouetteImage = character.gender === GENDERS.MALE ? manImage : womanImage;
   const isFavorite = favorites.some(fav => fav.id === character.id);
 
-  const toggleFavorite = () => {
+  const toggleFavorite = (event: SyntheticEvent) => {
+    // Stop event propagation to prevent navigation to details page
+    event.stopPropagation();
+
     if (isFavorite) {
       removeFavorite(character.id);
       toast(`${character.name} removed from favorites`);
@@ -24,9 +31,15 @@ export const CharacterCard = ({ character }: PropTypes) => {
     }
   };
 
-  console.log(isFavorite);
+  const navigateToDetail = () => {
+    navigate(`${ROUTES.CHARACTERS}/${character.id}`);
+  };
+
   return (
-    <li className='w-full mx-auto mt-1 rounded-lg p-0.5 shadow-lg border-2 border-gray-100'>
+    <li
+      onClick={navigateToDetail}
+      className='cursor-pointer w-full mx-auto mt-1 rounded-lg p-0.5 shadow-lg border-2 border-gray-100'
+    >
       <div className='bg-white p-7 rounded-md'>
         <img src={silhouetteImage} alt='Character gender representation' />
         <h2 className='font-bold text-xl mb-2'>{character.name}</h2>
