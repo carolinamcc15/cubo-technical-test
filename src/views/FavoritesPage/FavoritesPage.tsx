bf8f8aae5796810409edfbf55e9c798802173ed9
Carolina Carranza <carolina@tallerest.com>
2024-04-27 01:02:44 -0600

    chore: move layout common styles to each page to customize details page

--- src/views/FavoritesPage/FavoritesPage.tsx
+++ src/views/FavoritesPage/FavoritesPage.tsx
@@ -6,11 +6,12 @@ export const FavoritesPage = () => {
   const { favorites } = useFavoritesContext();
 
   return (
-    <main>
-      <h1 className='page-title'>Favorite characters</h1>
+    <main className='p-4 md:p-6 lg:p-10 max-w-[1400px] m-auto'>
+      <h1 className='page-title mb-2'>Favorite characters</h1>
+      <span className='mb-5'>{favorites.length} favorites found</span>
       <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5'>
         {favorites.map((character: ICharacter) => (
-          <CharacterCard character={character} />
+          <CharacterCard key={character.id} character={character} />
         ))}
       </ul>
     </main>
